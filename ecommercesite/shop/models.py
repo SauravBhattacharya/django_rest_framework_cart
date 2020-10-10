eae03f169360b1285d19c1cfbc0bb5df0eaf8dfc
sauravbhattacharya14 <sauravbhattacharya14@gmail.com>
2020-10-10 16:49:23 +0530

    My Project

---
+++ ecommercesite/shop/models.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ecommercesite import settings
+
+class User(AbstractUser):
+    social_media = models.CharField(max_length=255, default="", blank=True)
+
+class Product(models.Model):
+    """A model that contains data for a single product."""
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    title = models.CharField(max_length=255, default="")
+    available_inventory = models.PositiveIntegerField(default=0)
+
+class Cart(models.Model):
+    """A model that contains data for a shopping cart."""
+    customer = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name='cart',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    """A model that contains data for an item in the shopping cart."""
+    cart = models.ForeignKey(
+        Cart,
+        related_name='items',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    product = models.ForeignKey(
+        Product,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
+
+    def __unicode__(self):
+        return '%s: %s' % (self.product.title, self.quantity)
+
+class Order(models.Model):
+    """
+    An Order is the more permanent counterpart of the shopping cart. It represents
+    the frozen the state of the cart on the moment of a purchase. In other words,
+    an order is a customer purchase.
+    """
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='orders',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    total = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OrderItem(models.Model):
+    """A model that contains data for an item in an order."""
+    order = models.ForeignKey(
+        Order,
+        related_name='order_items',
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product,
+        related_name='order_items',
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return '%s: %s' % (self.product.title, self.quantity)
---
+++ ecommercesite/shop/serializers.py
@@ -0,0 +1,88 @@
+from shop.models import *
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'social_media')
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    """Serializer for the Product model."""
+
+    class Meta:
+        model = Product
+        fields = (
+            'id', 'price', 'title', 'available_inventory'
+        )
+
+
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    """Serializer for the Cart model."""
+
+    customer = UserSerializer(read_only=True)
+    # used to represent the target of the relationship using its __unicode__ method
+    items = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Cart
+        fields = [
+            'id', 'customer', 'created_at', 'updated_at', 'items'
+        ]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+
+    """Serializer for the CartItem model."""
+
+    cart = CartSerializer(read_only=True)
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = (
+            'id', 'cart', 'product', 'quantity'
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    """Serializer for the Order model."""
+
+    customer = UserSerializer(read_only=True)
+    # used to represent the target of the relationship using its __unicode__ method
+    order_items = serializers.StringRelatedField(many=True, required=False)
+
+    class Meta:
+        model = Order
+        fields = (
+            'id', 'customer', 'total', 'created_at', 'updated_at', 'order_items'
+        )
+
+    def create(self, validated_data):
+        """Override the creation of Order objects
+
+        Parameters
+        ----------
+        validated_data: dict
+
+        """
+        order = Order.objects.create(**validated_data)
+        return order
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    """Serializer for the OrderItem model."""
+
+    order = OrderSerializer(read_only=True)
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            'id', 'order', 'product', 'quantity'
+        )
